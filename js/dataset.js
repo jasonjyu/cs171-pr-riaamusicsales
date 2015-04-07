7a4b679d5cf24f7faa4581912801caf67a324fa3
Jason Yu <jasonyu@fas.harvard.edu>
2015-04-07 02:13:00 -0400

    Reverted data processing algorithm

--- js/dataset.js
+++ js/dataset.js
@@ -2,15 +2,15 @@
  * Dataset object for containing the different music sales data types.
  * @constructor
  * @param {array} _rows_units
- * @param {array} _rows_dollarValue
- * @param {array} _rows_inflatedDollarValue
+ * @param {array} _rows_dollars
+ * @param {array} _rows_inflatedDollars
  * @returns {undefined}
  */
-Dataset = function(_rows_units, _rows_dollarValue, _rows_inflatedDollarValue) {
+Dataset = function(_rows_units, _rows_dollars, _rows_inflatedDollars) {
 
     this.data_units = this.parseRows(_rows_units);
-    this.data_dollarValue = this.parseRows(_rows_dollarValue);
-    this.data_inflatedDollarValue = this.parseRows(_rows_inflatedDollarValue);
+    this.data_dollars = this.parseRows(_rows_dollars);
+    this.data_inflatedDollars = this.parseRows(_rows_inflatedDollars);
 };
 
 /**
@@ -19,18 +19,11 @@ Dataset = function(_rows_units, _rows_dollarValue, _rows_inflatedDollarValue) {
  *     formatName: , // {string} (CD, cassette, vinyl, download single,
  *                   //           paid subscriptions, etc.)
  *     formatType: , // {string} (physical, digital, streaming)
- *     sales:        // {array}  array of sale objects
- *     [
- *         {
- *             year: ,       // {number} year of the sales metric value
- *             value:        // {number} value of the sales metric (units,
- *                           //          dollar value, dollar value adjusted
- *                           //          for inflation)
- *         },
- *         ...
- *     ]
+ *     year:       , // {number} year of the sales metric value
+ *     value:        // {number} value of the sales metric (units, dollars,
+ *                   //          dollars adjusted for inflation)
  * }
- * @param {array} rows
+ * @param {array} rows -- music sales data rows
  * @returns {array} array of objects containing the restructured data
  */
 Dataset.prototype.parseRows = function(rows) {
@@ -39,23 +32,18 @@ Dataset.prototype.parseRows = function(rows) {
     rows.forEach(function(row) {
         // check for valid data
         if (row.formatName.trim() !== "" && row.formatType.trim() !== "") {
-            var sales = [];
             for (var key in row) {
                 // if the key is a number type,
                 // then save the sales metric value for that year
                 if (+key) {
-                    sales.push({
-                        year:  +key,
-                        value: +row[key]
+                    result.push({
+                        formatName:  row.formatName,
+                        formatType:  row.formatType,
+                        year:        +key,
+                        value:       +row[key]
                     });
                 }
             }
-
-            result.push({
-                formatName: row.formatName,
-                formatType: row.formatType,
-                sales:      sales
-            });
         }
     });
 
