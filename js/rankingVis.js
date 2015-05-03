8b9b20edb86a6a99bb6f4e4f99d9a09f4c3559b3
Jason Yu <jasonyu@fas.harvard.edu>
2015-05-03 02:30:30 -0400

    Added description to Rankings Chart

--- js/rankingVis.js
+++ js/rankingVis.js
@@ -122,15 +122,12 @@ RankingVis.prototype.updateVis = function(_options){
     // else{
     //     this.xScale.domain(d3.keys(this.colorMap2))
     //     } 
-    yMin = d3.min(this.displayData, function(d){
+    yMin = Math.min(0, d3.min(this.displayData, function(d){
         return d.value;
-       })
+       }));
     yMax = d3.max(this.displayData, function(d){
         return d.value;
        });
-    if (yMin == yMax) {
-        yMin = 0
-    }
     this.yScale.domain([yMin, yMax]);
 
     // update axis
